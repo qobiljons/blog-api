ebd466715f62435c3a8d6289ec2af3554eca0cf0
Qobiljon Xayrullayev <qobiljonkhayrullayev@gmail.com>
2024-07-13 21:21:01 +0500

    adding tag model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.label
+    
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+    def __str__(self):
+        return f"{self.tag} - {self.content_object}"
+    
+
+
